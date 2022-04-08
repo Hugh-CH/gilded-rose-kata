3ee4a94144c288e0df1a933f8e241fa294af828b
HugHud <hugh.hudson@softwire.com>
2022-04-08 14:48:47 +0100

    test: Add brie tests

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -6,9 +6,11 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
-  it("Should increase brie quality", function() {
-    const gildedRose = new Shop([new Item('Aged Brie', 5, 0)]);
+  it("Should increase brie quality but not above 50", function() {
+    const gildedRose = new Shop([new Item('Aged Brie', 5, 0),new Item('Aged Brie', 5, 49),new Item('Aged Brie', 5, 50)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(items[0].quality).toBe(1);
+    expect(items[1].quality).toBe(50);
+    expect(items[2].quality).toBe(50);
   });
 });
