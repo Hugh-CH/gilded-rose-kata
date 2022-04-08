105cadd0832a05acec0fc1b9b283e6b109964341
HugHud <hugh.hudson@softwire.com>
2022-04-08 15:11:42 +0100

    test: Add tickets tests

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -42,3 +42,26 @@ describe('Gilded Rose', function() {
     expect(items[2].quality).toBe(20);
   });
 });
+describe('Backstage Tests', function() {
+  it('Should increase by 1 if there are more than 10 days left', function() {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 0),new Item('Backstage passes to a TAFKAL80ETC concert', 12, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
+    expect(items[1].quality).toBe(50);
+  });
+  it('Should increase by 2 if there are between 10 and 5 days left', function() {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 7, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(2);
+  });
+  it('Should increase by 3 if there are less than 5 days left', function() {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 3, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+  });
+  it('Should be zero if there are no days left', function() {
+    const gildedRose = new Shop([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
