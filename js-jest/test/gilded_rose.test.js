332e79ad00d37a1ddd8bf6840616ca8ee572d117
HugHud <hugh.hudson@softwire.com>
2022-04-08 16:00:35 +0100

    test: More Tests

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -6,19 +6,24 @@ describe('Gilded Rose', function() {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
   });
-  it('Should increase brie quality by one but not above 50 if before sell by date', function() {
+  it('Should increase brie quality by one if before sell by date but not above 50 ', function() {
     const gildedRose = new Shop([new Item('Aged Brie', 5, 0),new Item('Aged Brie', 5, 49),new Item('Aged Brie', 5, 50)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(1);
     expect(items[1].quality).toBe(50);
     expect(items[2].quality).toBe(50);
   });
-  it('Should increase brie quality by two but not above 50 if after sell by date', function() {
-    const gildedRose = new Shop([new Item('Aged Brie', -2, 0),new Item('Aged Brie', 0, 49),new Item('Aged Brie', 0, 50)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(2);
+  it('Should increase brie quality by two if after sell by date but not above 50 ', function() {
+    const gildedRose = new Shop([new Item('Aged Brie', 1, 0),new Item('Aged Brie', 0, 49),new Item('Aged Brie', 0, 50)]);
+    let items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
     expect(items[1].quality).toBe(50);
     expect(items[2].quality).toBe(50);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(3);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(5);
+
   });
   it('Should decrease quality by one but not below zero', function() {
     const gildedRose = new Shop([new Item('Test Item', 2, 0),new Item('Test Item', 2, 1),new Item('Test Item', 2, 20)]);
@@ -28,11 +33,18 @@ describe('Gilded Rose', function() {
     expect(items[2].quality).toBe(19);
   });
   it('Should decrease quality by two but not below zero if past sell by date', function() {
-    const gildedRose = new Shop([new Item('Test Item', 0, 0),new Item('Test Item', -2, 1),new Item('Test Item', -2, 20)]);
-    const items = gildedRose.updateQuality();
+    const gildedRose = new Shop([new Item('Test Item', 1, 0),new Item('Test Item', 1, 2),new Item('Test Item', 1, 20)]);
+    let items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(0);
+    expect(items[0].sellIn).toBe(0);
+    expect(items[1].quality).toBe(1);
+    expect(items[2].quality).toBe(19);
+    items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+    expect(items[0].sellIn).toBe(-1);
     expect(items[1].quality).toBe(0);
-    expect(items[2].quality).toBe(18);
+    expect(items[2].quality).toBe(17);
+
   });
   it('Should not update Sulfuras, Hand of Ragnaros', function() {
     const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 12, 0),new Item('Sulfuras, Hand of Ragnaros', 0, 1),new Item('Sulfuras, Hand of Ragnaros', 0, 20)]);
