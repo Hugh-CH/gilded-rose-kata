dd3cea6739e679d345cb8f7a76f2b50e4a8702ec
HugHud <hugh.hudson@softwire.com>
2022-04-22 15:05:23 +0100

    refactor: Extract separate methods for each item type

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -4,47 +4,68 @@ class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  updateSellIn () {
+    if (this.name !== 'Sulfuras, Hand of Ragnaros'){
+      this.sellIn=this.sellIn-1;
+    }
+  }
 }
 
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  getQuality({name, quality, sellIn}){
-    if (name === 'Aged Brie' || name === 'Backstage passes to a TAFKAL80ETC concert') {
-      if (quality < 50) {
-        quality += 1;
-        if (name === 'Backstage passes to a TAFKAL80ETC concert') {
-          if (sellIn < 11) {
-            quality +=1;
-          }
-          if (sellIn < 6) {
-            quality += 1 ;
-          }
-          if (sellIn < 0) {
-            return 0;
-          }
-        }
-      }
-    } else if (quality > 0) {
-      quality -= 1;
-    }
 
-    if (sellIn < 0) {
-      if ((name === 'Aged Brie' || name === 'Backstage passes to a TAFKAL80ETC concert') && quality < 50) {
-        quality += 1;
-      } else if (quality > 0) {
-        quality -= 1;
-      }
-    }
-    return quality;
+  updateStandardItemQuality(item){
+    const reducedQuality = (item.sellIn >= 0) ? item.quality-1 : item.quality-2;
+    const newQuality = (reducedQuality < 0) ? 0 : reducedQuality;
+    return new Item(item.name,item.sellIn,newQuality)
   }
 
+  updateBackStagePassQuality(item){
+    if (item.sellIn > 10) {
+      item.quality+=1;
+    }else if (item.sellIn <= 10 && item.sellIn >5){
+      item.quality+=2;
+    }else if (item.sellIn <= 5 && item.sellIn >= 0){
+      item.quality+=3;
+    }else
+      item.quality = 0;
+
+    if (item.quality > 50){
+      item.quality = 50;
+    }
+    return item;
+  }
+
+  updateBrieQuality(item){
+    const increasedQuality = (item.sellIn >= 0) ? item.quality+1 : item.quality+2;
+    const newQuality = (increasedQuality > 50) ? 50 : increasedQuality;
+    return new Item(item.name,item.sellIn,newQuality)}
+
   updateItems() {
+
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') break;
-      this.items[i].sellIn = this.items[i].sellIn - 1;
-      this.items[i].quality = this.getQuality(this.items[i]);
+
+      this.items[i].updateSellIn();
+
+      switch (this.items[i].name){
+        case 'Sulfuras, Hand of Ragnaros':
+          console.log('Sulfuras, Hand of Ragnaros');
+          break;
+
+        case 'Aged Brie':
+          this.items[i] = this.updateBrieQuality(this.items[i]);
+          break;
+
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.items[i] = this.updateBackStagePassQuality(this.items[i]);
+          break;
+
+        default:
+          this.items[i] = this.updateStandardItemQuality(this.items[i]);
+      }
     }
 
     return this.items;
