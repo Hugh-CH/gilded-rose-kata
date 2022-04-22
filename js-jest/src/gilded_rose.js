3da9cf0a54e0e0623a6b8b3fbe0a3e5f2f9c85d8
HugHud <hugh.hudson@softwire.com>
2022-04-22 15:25:30 +0100

    refactor: Put methods into item class

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,6 +10,53 @@ class Item {
       this.sellIn=this.sellIn-1;
     }
   }
+
+  updateQuality () {
+    switch (this.name){
+    case 'Sulfuras, Hand of Ragnaros':
+      break;
+
+    case 'Aged Brie':
+      this.updateBrieQuality();
+      break;
+
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      this.updateBackStagePassQuality();
+      break;
+
+    default:
+      this.updateStandardItemQuality();
+    }
+  }
+
+  updateBrieQuality(){
+    this.quality = (this.sellIn >= 0) ? this.quality+1 : this.quality+2;
+    if (this.quality>50){
+      this.quality=50;
+    }
+  }
+
+  updateStandardItemQuality(){
+    this.quality = (this.sellIn >= 0) ? this.quality-1 : this.quality-2;
+    if (this.quality < 0){
+      this.quality = 0;
+    }
+  }
+
+  updateBackStagePassQuality(){
+    if (this.sellIn > 10) {
+      this.quality+=1;
+    } else if (this.sellIn <= 10 && this.sellIn >5){
+      this.quality+=2;
+    } else if (this.sellIn <= 5 && this.sellIn >= 0){
+      this.quality+=3;
+    } else
+      this.quality = 0;
+
+    if (this.quality > 50){
+      this.quality = 50;
+    }
+  }
 }
 
 class Shop {
@@ -17,55 +64,12 @@ class Shop {
     this.items = items;
   }
 
-  updateStandardItemQuality(item){
-    const reducedQuality = (item.sellIn >= 0) ? item.quality-1 : item.quality-2;
-    const newQuality = (reducedQuality < 0) ? 0 : reducedQuality;
-    return new Item(item.name,item.sellIn,newQuality)
-  }
-
-  updateBackStagePassQuality(item){
-    if (item.sellIn > 10) {
-      item.quality+=1;
-    }else if (item.sellIn <= 10 && item.sellIn >5){
-      item.quality+=2;
-    }else if (item.sellIn <= 5 && item.sellIn >= 0){
-      item.quality+=3;
-    }else
-      item.quality = 0;
-
-    if (item.quality > 50){
-      item.quality = 50;
-    }
-    return item;
-  }
-
-  updateBrieQuality(item){
-    const increasedQuality = (item.sellIn >= 0) ? item.quality+1 : item.quality+2;
-    const newQuality = (increasedQuality > 50) ? 50 : increasedQuality;
-    return new Item(item.name,item.sellIn,newQuality)}
-
   updateItems() {
 
     for (let i = 0; i < this.items.length; i++) {
 
       this.items[i].updateSellIn();
-
-      switch (this.items[i].name){
-        case 'Sulfuras, Hand of Ragnaros':
-          console.log('Sulfuras, Hand of Ragnaros');
-          break;
-
-        case 'Aged Brie':
-          this.items[i] = this.updateBrieQuality(this.items[i]);
-          break;
-
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          this.items[i] = this.updateBackStagePassQuality(this.items[i]);
-          break;
-
-        default:
-          this.items[i] = this.updateStandardItemQuality(this.items[i]);
-      }
+      this.items[i].updateQuality();
     }
 
     return this.items;
